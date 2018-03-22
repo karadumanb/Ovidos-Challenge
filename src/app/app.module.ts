c8e1a3c5056baaaafc9188a182cc59a4d68563d3
karadumanb <karadumanbaturay@gmail.com>
2018-03-22 22:27:35 +0300

    Create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    UsersComponent,
+    SidebarComponent,
+    AddUserComponent,
+    EditUserComponent,
+    UserDetailsComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
